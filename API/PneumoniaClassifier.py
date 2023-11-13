39c7d46a06cb95d51a345e3b39c8f7d888db8c56
Harsha <khurdhulaharshavardhan@gmail.com>
2023-11-12 19:34:57 -0500

    Creates a Pneumonia Classifier module.

---
+++ API/PneumoniaClassifier.py
@@ -0,0 +1,12 @@
+"""
+PneumoniaClassifier
+- Harsha Vardhan, Khurdula
+hkhurdul@purdue.edu
+"""
+
+
+class VGG16():
+    """
+    This module acts as a middleware between the endpoint (Flask-api) and the Binary of the model.
+    """
+    pass
